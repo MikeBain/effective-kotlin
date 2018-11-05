06c592e5581fca6f98676d3b16c23094e9fffeca
Mike Bain <mikebain@squareup.com>
2018-11-05 15:37:37 +1100

    Add new functions page

--- src/main/kotlin/02-functions.kt
+++ src/main/kotlin/02-functions.kt
@@ -1,3 +1,21 @@
-fun isPhoneNumber(s: String): Boolean {
-  return s.length == 7
+typealias Coffee = String // Never do this
+
+fun isRealCoffee(name: Coffee): Boolean {
+  fun nameSoundsReal(s: Coffee): Boolean {
+    return s.toLowerCase() in arrayOf("flat white", "long black")
+  }
+  fun nameSoundsAmerican(s: Coffee): Boolean {
+    return s.contains("Starbucks")
+  }
+  fun nameSoundsLikeFakeItalian(s: Coffee) = true
+
+  return nameSoundsReal(name) && !nameSoundsAmerican(name) && !nameSoundsLikeFakeItalian(name)
+}
+
+fun main(args: Array<Coffee>) {
+  val name = "Starbucks Flat Whitiato"
+  println("Is $name real coffee? ${if(isRealCoffee(name)) "yes!" else "no :-("}")
+  TODO("Implement nameSoundsLikeFakeItalian")
+  TODO("Turn subfunctions into expression functions")
+  TODO("Make isRealCoffee an extension method of Coffee 👹")
 }
